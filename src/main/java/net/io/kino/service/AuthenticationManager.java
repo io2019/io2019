089d9ed2e410418d2d2833d0d77c8b4be4cdf72d
anitTer1789 <49729208+anitTer1789@users.noreply.github.com>
2020-01-18 18:54:06 +0100

    changed login declaration

--- src/main/java/net/io/kino/service/AuthenticationManager.java
+++ src/main/java/net/io/kino/service/AuthenticationManager.java
@@ -1,8 +1,6 @@
 package net.io.kino.service;
 
-import org.springframework.security.core.Authentication;
-
 public interface AuthenticationManager {
-    Authentication login(String username, String password);
+    boolean login(String username, String password);
     boolean authenticate(String username, String password);
 }
