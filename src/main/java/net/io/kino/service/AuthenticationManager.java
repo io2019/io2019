7cc1b7b0230afa91240fda5269c72b363206756a
anitTer1789 <49729208+anitTer1789@users.noreply.github.com>
2020-01-18 11:54:02 +0100

    Update AuthenticationManager
    deleted thrown exceptions

--- src/main/java/net/io/kino/service/AuthenticationManager.java
+++ src/main/java/net/io/kino/service/AuthenticationManager.java
@@ -1,13 +1,6 @@
 package net.io.kino.service;
 
-import net.io.kino.model.Manager;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
-
 public interface AuthenticationManager {
-    boolean login(String username, String password) throws InvalidKeySpecException, NoSuchAlgorithmException;
-    boolean logout(Manager user);
-    boolean authenticate(String username, String password) throws InvalidKeySpecException, NoSuchAlgorithmException;
+    boolean login(String username, String password);
+    boolean authenticate(String username, String password);
 }
