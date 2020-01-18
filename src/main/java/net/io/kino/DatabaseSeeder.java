839440284025cdfbe1cb0c73cc263eddc46603aa
Adikso <adam@zambrzycki.net>
2020-01-18 23:59:53 +0100

    Add seeding managers table

---
+++ src/main/java/net/io/kino/DatabaseSeeder.java
@@ -0,0 +1,27 @@
+package net.io.kino;
+
+import net.io.kino.model.Manager;
+import net.io.kino.repository.ManagersRepository;
+import net.io.kino.service.impl.AuthenticationManagerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder {
+
+    @Autowired
+    private ManagersRepository managersRepository;
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        if (!managersRepository.existsById("admin")) {
+            Manager manager = new Manager();
+            manager.setUsername("admin");
+            manager.setPassword(AuthenticationManagerImpl.getSaltedHash("dupa"));
+            managersRepository.save(manager);
+        }
+    }
+
+}
