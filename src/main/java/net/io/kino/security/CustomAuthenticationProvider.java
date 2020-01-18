ce8f9b676c20784f96754655e17ab322d69e3b65
anitTer1789 <49729208+anitTer1789@users.noreply.github.com>
2020-01-18 19:38:34 +0100

    Finish authorization module (#6)
    * add User, Manager Repository and AuthenticationMan
    
    * deleted User.java
    
    * added Manager.java
    
    added field "username". Changed fields to private. Fields status and ID are currently deleted
    
    * changes made in ManagersRepository
    
    * changed AuthenticationManger
    
    changes in methods declarations
    
    * changed AuthenticationManagerImpl
    
    changed methods according to the declarations in interface
    todo - passing login tokens and logout method
    
    * Changed thrown Exceptions
    
    Switched Exception for InvalidKeySpecException and NoSuchAlgorithmException
    
    * deleted ActionLogger
    
    file was added by accident
    
    * deleted LogWriter
    
    file was added by accident
    
    * deleted ReservationService
    
    file was added by accident
    
    * deleted CinemaActionImpl
    
    file was added by accident
    
    * delted LocalDestinationLogWriterImpl
    
    file was added by accident
    
    * delted ReservationServiceImpl
    
    file was added by accident
    
    * Revert deletion of ReservationService
    
    * Revert deletion of ReservationService v2
    
    * Update AuthenticationManager
    
    deleted thrown exceptions
    
    * updated AuthenticationManagerImpl
    
    using try-catch blocks instead of throwing exceptions
    
    * added SecurityConfiguration
    
    * Update AuthenticationManagerImpl.java
    
    * added spring security dependency
    
    * changed login method declaration
    
    * changed login method
    
    * updated SecurityConfiguration, added CustomAuthProvider
    
    * changed login declaration
    
    * changed login method
    
    * added @Component annotation
    
    * added component annotation
    
    * added Autowired annotation
    
    * Fix login service
    
    * Fix formatting
    
    Co-authored-by: Adam Zambrzycki <adam@zambrzycki.net>

---
+++ src/main/java/net/io/kino/security/CustomAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package net.io.kino.security;
+
+import net.io.kino.service.AuthenticationManager;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private AuthenticationManager auth;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+
+        try {
+            String name = authentication.getName();
+            String password = authentication.getCredentials().toString();
+
+            if (auth.login(name, password)) {
+                return new UsernamePasswordAuthenticationToken(
+                        name, password, new ArrayList<>());
+            } else {
+                return null;
+            }
+        } catch (AuthenticationException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
---
+++ src/main/java/net/io/kino/service/impl/AuthenticationManagerImpl.java
@@ -0,0 +1,75 @@
+package net.io.kino.service.impl;
+
+import net.io.kino.repository.ManagersRepository;
+import net.io.kino.model.Manager;
+import net.io.kino.service.AuthenticationManager;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+@Service
+public class AuthenticationManagerImpl implements AuthenticationManager {
+
+    private static final int iterations = 20 * 1000;
+    private static final int saltLen = 32;
+    private static final int desiredKeyLen = 256;
+
+    @Autowired
+    private ManagersRepository managers;
+
+    @Override
+    public boolean login(String username, String password) {
+        return authenticate(username, password);
+    }
+
+    // check if hash of a given password corresponds with a password in database
+    @Override
+    public boolean authenticate(String username, String password) {
+        Manager found = managers.getOne(username);
+        String stored = found.getPassword();
+
+        String[] saltAndHash = stored.split("\\$");
+        if (saltAndHash.length != 2) {
+            throw new IllegalStateException(
+                    "Stored password doesn't have a form 'salt$hash'");
+        }
+        String hashOfInput = hash(password, Base64.decodeBase64(saltAndHash[0]));
+        return hashOfInput.equals(saltAndHash[1]);
+    }
+
+    // compute salted PBKDF2 hash of given password
+    public static String getSaltedHash(String password) {
+        byte[] salt = new byte[0];
+
+        try {
+            salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex.getMessage());
+        }
+        // store the salt with the password
+        return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
+    }
+
+    private static String hash(String password, byte[] salt) {
+        SecretKey key = null;
+
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            key = f.generateSecret(new PBEKeySpec(
+                    password.toCharArray(), salt, iterations, desiredKeyLen));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        assert key != null;
+        return Base64.encodeBase64String(key.getEncoded());
+    }
+
+}
