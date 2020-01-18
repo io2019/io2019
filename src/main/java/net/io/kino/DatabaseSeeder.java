3f58f8a1c731359f940b94301efb1f2a84375f0d
Adikso <adam@zambrzycki.net>
2020-01-19 00:14:35 +0100

    Add ticket types seeder

--- src/main/java/net/io/kino/DatabaseSeeder.java
+++ src/main/java/net/io/kino/DatabaseSeeder.java
@@ -1,7 +1,9 @@
 package net.io.kino;
 
 import net.io.kino.model.Manager;
+import net.io.kino.model.TicketType;
 import net.io.kino.repository.ManagersRepository;
+import net.io.kino.repository.TicketTypesRepository;
 import net.io.kino.service.impl.AuthenticationManagerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -14,6 +16,9 @@ public class DatabaseSeeder {
     @Autowired
     private ManagersRepository managersRepository;
 
+    @Autowired
+    private TicketTypesRepository ticketTypesRepository;
+
     @EventListener
     public void seed(ContextRefreshedEvent event) {
         if (managersRepository.findAll().isEmpty()) {
@@ -21,6 +26,14 @@ public class DatabaseSeeder {
             manager.setUsername("admin");
             manager.setPassword(AuthenticationManagerImpl.getSaltedHash("dupa"));
             managersRepository.save(manager);
+        }
+
+        if (ticketTypesRepository.findAll().isEmpty()) {
+            TicketType normalTicket = new TicketType("Normal", 26, true);
+            ticketTypesRepository.save(normalTicket);
+
+            TicketType reducedTicket = new TicketType("Reduced", 21, true);
+            ticketTypesRepository.save(reducedTicket);
         }
     }
 
