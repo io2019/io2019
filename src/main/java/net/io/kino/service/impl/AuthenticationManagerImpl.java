d70d436af005c92563cc939bbc25870cdc518dc0
anitTer1789 <49729208+anitTer1789@users.noreply.github.com>
2020-01-18 11:57:24 +0100

    updated AuthenticationManagerImpl
    using try-catch blocks instead of throwing exceptions

--- src/main/java/net/io/kino/service/impl/AuthenticationManagerImpl.java
+++ src/main/java/net/io/kino/service/impl/AuthenticationManagerImpl.java
@@ -3,6 +3,7 @@ package net.io.kino.service.impl;
 import net.io.kino.repository.ManagersRepository;
 import net.io.kino.model.Manager;
 import net.io.kino.service.AuthenticationManager;
+import net.io.kino.security.SecurityConfiguration;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,6 +16,7 @@ import java.security.spec.InvalidKeySpecException;
 
 public class AuthenticationManagerImpl implements AuthenticationManager {
 
+    SecurityConfiguration sc = new SecurityConfiguration();
     private static final int iterations = 20*1000;
     private static final int saltLen = 32;
     private static final int desiredKeyLen = 256;
@@ -23,18 +25,14 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
     private ManagersRepository managers;
 
     @Override
-    public boolean login(String username, String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public boolean login(String username, String password) {
         return authenticate(username, password);
     }
 
-    @Override
-    public boolean logout(Manager user) {
-        return false;
-    }
 
     // check if hash of a given password corresponds with a password in database
     @Override
-    public boolean authenticate(String username, String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public boolean authenticate(String username, String password) {
         Manager found = managers.getOne(username);
         String stored = found.getPassword();
 
@@ -48,19 +46,31 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
     }
 
     // compute salted PBKDF2 hash of given password
-    public static String getSaltedHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+    public static String getSaltedHash(String password) {
+        byte[] salt = new byte[0];
+
+        try{
+           salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex.getMessage());
+        }
         // store the salt with the password
         return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
     }
 
 
-    private static String hash(String password, byte[] salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
-        if (password == null || password.length() == 0)
-            throw new IllegalArgumentException("Empty passwords are not supported.");
-        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        SecretKey key = f.generateSecret(new PBEKeySpec(
-                password.toCharArray(), salt, iterations, desiredKeyLen));
+    private static String hash(String password, byte[] salt){
+        SecretKey key = null;
+
+        try{
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            key = f.generateSecret(new PBEKeySpec(
+                    password.toCharArray(), salt, iterations, desiredKeyLen));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        assert key != null;
         return Base64.encodeBase64String(key.getEncoded());
     }
 
