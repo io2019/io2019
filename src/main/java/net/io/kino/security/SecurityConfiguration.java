0214c1511d279a233f79d3a42da5d0330fc4b7c1
anitTer1789 <49729208+anitTer1789@users.noreply.github.com>
2020-01-18 11:58:51 +0100

    added SecurityConfiguration

---
+++ src/main/java/net/io/kino/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package net.io.kino.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .authoritiesByUsernameQuery("select username, 'admin' FROM users where username=?")
+                .usersByUsernameQuery("select username, concat('{noop}', password), 1 FROM users where username=?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+        http.csrf().disable();
+    }
+
+}
