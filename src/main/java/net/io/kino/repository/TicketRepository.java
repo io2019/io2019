7765a2a0e95de354ab6992a9272baec53afd3ecb
Adikso <adam@zambrzycki.net>
2020-01-20 05:36:47 +0100

    Add TicketRepository

---
+++ src/main/java/net/io/kino/repository/TicketRepository.java
@@ -0,0 +1,13 @@
+package net.io.kino.repository;
+
+import net.io.kino.model.Showtime;
+import net.io.kino.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> getTicketsByShowtime(Showtime showtime);
+}
